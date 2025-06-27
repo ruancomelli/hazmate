071605f57e51034d6e6063f7050369f04d699564
Ruan Comelli <ruancomelli@gmail.com>
2025-06-26 21:12:22 -0300

    Initialize Hazmat attribute evaluator

---
+++ src/hazmate/evaluation/attribute_evaluation_config.py
@@ -0,0 +1,37 @@
+from pydantic import BaseModel, ConfigDict
+
+
+class HazmatAttribute(BaseModel):
+    name: str
+    value: str
+    tags: list[str] = []
+
+
+class AttributeEvaluationConfig(BaseModel):
+    """Configuration for attribute evaluation.
+
+    Define the attributes that are considered hazmat. For example, a product
+    that has the attribute "É inflamável: Sim" is considered hazmat.
+
+    ```yaml
+    hazmat_attributes:
+    - name: É inflamável
+      value: Sim
+      tags:
+      - INFLAMABLE
+
+    - name: Com mercúrio
+      value: Sim
+      tags:
+      - MERCURY
+
+    - name: Tipo de termômetro
+      value: De mercúrio
+      tags:
+      - MERCURY
+    ```
+    """
+
+    model_config = ConfigDict(extra="forbid", frozen=True)
+
+    hazmat_attributes: list[HazmatAttribute] = []
